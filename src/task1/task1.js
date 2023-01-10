4d7b0e795a2e6943af53e1b48e54ded3b3ce86f1
sergey-salo <ssnonmail@gmail.com>
2023-01-10 20:18:07 +0200

    added functionality of task 1

--- src/task1/task1.js
+++ src/task1/task1.js
@@ -9,6 +9,8 @@
 
 function isNumberArray(arr) {
   // write code here
+  return arr && arr.length ? arr.every((item) => (!isNaN(item) && typeof item === "number")) : false;
+
 }
 
 module.exports = isNumberArray;
