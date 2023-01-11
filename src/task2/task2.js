e5cbd393bbb6a831090e8770c2be29d5cc2cfa05
user.Oleksandr <user.oleksandr@gmail.com>
2023-01-11 17:48:25 +0200

    task_2

--- src/task2/task2.js
+++ src/task2/task2.js
@@ -24,10 +24,48 @@
  * @param {Array} arr
  *
  * @return {Array}
-*/
+ */
 
 function plusOneArray(arr) {
-  // write code here
-}
+  if (!isInputIsNonEmptyArray(arr)) {
+    return null;
+  }
 
+  const isNumber = num => typeof num === 'number';
+  const isIntSingleDigit = num => Number.isInteger(num) && num >= 0 && num < 10;
+
+  let resultArr = [];
+  let i = arr.length;
+  let num;
+  while (i-- > 0) {
+    num = arr[i];
+    if (!isNumber(num) || !isIntSingleDigit(num)) {
+      return null;
+    }
+
+    if (num === 9) {
+      resultArr[i] = 0;
+      if (i === 0) {
+        resultArr.unshift(1);
+        break;
+      }
+    } else {
+      resultArr[i] = num + 1;
+      while (--i > -1) {
+        num = arr[i];
+        if (!isNumber(num) || !isIntSingleDigit(num)) {
+          return null;
+        }
+        resultArr[i] = arr[i];
+      }
+      break;
+    }
+  }
+
+  return resultArr;
+
+  function isInputIsNonEmptyArray(arr) {
+    return Array.isArray(arr) && arr.length > 0;
+  }
+}
 module.exports = plusOneArray;
