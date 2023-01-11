60d01901bd136df1caffe5464e4866596c127832
ArtemBorshchev <artemborshchev@icloud.com>
2023-01-11 23:57:49 +0200

    task1 & task3 small correkted

--- src/task1/task1.js
+++ src/task1/task1.js
@@ -12,17 +12,13 @@ function isNumberArray(arr) {
   if ((Array.isArray(arr) && !arr.length) || !arr) {
     return false;
   }
-  let count = 0;
   for (let i = 0; i < arr.length; i += 1) {
-    if (typeof (arr[i]) !== typeof (1) || Number.isNaN(arr[i])) {
-      count += 1;
-
+    if (typeof (arr[i]) !== typeof (NaN) || Number.isNaN(arr[i])) {
       return false;
     }
   }
-  if (count === 0) {
-    return true;
-  }
+
+  return true;
 }
 
 module.exports = isNumberArray;
