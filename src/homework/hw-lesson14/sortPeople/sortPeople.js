8fa41edcda827a3a7eab5bf4975820cb7d37fc86
oleksii onyshchuk <oleksii.onyshchuk@additiv.com>
2023-01-21 12:54:05 +0200

    add homework 14

---
+++ src/homework/hw-lesson14/sortPeople/sortPeople.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ *
+ * Write three functions
+ *
+ * @param {arr} Array
+ *
+ * @return {Array}
+*/
+
+/** Sort by age */
+function sortByAge(arr) {
+  // write code here
+}
+
+/** Sort by name */
+function sortByName(arr) {
+  // write code here
+}
+
+/** Sort by children quantity */
+function sortByChildrenQuantity(arr) {
+  // write code here
+}
+
+module.exports = {
+  sortByAge,
+  sortByName,
+  sortByChildrenQuantity,
+};
