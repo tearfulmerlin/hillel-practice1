67f090512baa1e29e5db505d14e9a248ed7d04ee
Volodymyr Vyshniakov <wwwowka@gmail.com>
2023-01-24 11:06:42 +0200

    recieve string

--- src/homework/hw-lesson14/sortPeople/sortPeople.js
+++ src/homework/hw-lesson14/sortPeople/sortPeople.js
@@ -12,19 +12,19 @@
 /** Sort by age */
 function sortByAge(arr) {
   // write code here
-  return arr.sort((a, b) => (a.died - a.born) - (b.died - b.born));
+  return [...arr].sort((a, b) => (a.died - a.born) - (b.died - b.born));
 }
 
 /** Sort by name */
 function sortByName(arr) {
   // write code here
-  arr.sort((a, b) => a.name.localeCompare(b.name));
+  return [...arr].sort((a, b) => a.name.localeCompare(b.name));
 }
 
 /** Sort by children quantity */
 function sortByChildrenQuantity(arr) {
   // write code here
-  const sortedArr = arr.sort((pplA, pplB) => {
+  const sortedArr = [...arr].sort((pplA, pplB) => {
     const numChildrenA = arr.filter((person) => person
       .father === pplA.name || person.mother === pplA.name);
 
