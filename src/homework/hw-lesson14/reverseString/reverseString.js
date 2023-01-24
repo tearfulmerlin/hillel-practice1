02911d274bd0be0815ed1b837bdcf37105fca1f5
Oleksandr Anastasiiev <name.oleksandr@gmail.com>
2023-01-24 13:37:23 +0200

    task_filter/reverse/sort

--- src/homework/hw-lesson14/reverseString/reverseString.js
+++ src/homework/hw-lesson14/reverseString/reverseString.js
@@ -15,7 +15,9 @@
  */
 
 function reverseString(str) {
+
   if (typeof str === 'string') {
+
     return str.split('').reverse().join('');
   }
   return null;
--- src/homework/hw-lesson14/sortPeople/sortPeople.js
+++ src/homework/hw-lesson14/sortPeople/sortPeople.js
@@ -7,22 +7,52 @@
  * @param {arr} Array
  *
  * @return {Array}
-*/
+ */
 
 /** Sort by age */
 function sortByAge(arr) {
-  // write code here
+  const arrSortAge = [...arr];
+
+  arrSortAge.sort((a, b) => {
+    const peopleA = a.born - a.died;
+    const peopleB = b.born - b.died;
+
+    return peopleB - peopleA;
+  });
+  return arrSortAge;
 }
+
 
 /** Sort by name */
 function sortByName(arr) {
-  // write code here
+  const arrSortName = [...arr];
+
+  arrSortName.sort((a, b) => a.name.localeCompare(b.name));
+
+  return arrSortName;
 }
 
+
 /** Sort by children quantity */
+// eslint-disable-next-line no-unused-vars
 function sortByChildrenQuantity(arr) {
-  // write code here
+
+  const sorted = [...arr];
+
+  sorted.sort((a, b) => {
+    const quantityA = arr.filter((person) => {
+      return person.father === a.name || person.mother === a.name;
+    });
+    const quantityB = arr.filter((person) => {
+      return person.father === b.name || person.mother === b.name;
+    });
+
+    return (quantityA.length - quantityB.length);
+  });
+
+  return sorted;
 }
+
 
 module.exports = {
   sortByAge,
