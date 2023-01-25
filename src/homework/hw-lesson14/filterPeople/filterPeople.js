d07bedda12312d174fcaaf0b10b96e97e5b31558
Hladiy Taras <tarik2002sta@gmail/com>
2023-01-25 13:58:10 +0200

    6 passed

--- src/homework/hw-lesson14/filterPeople/filterPeople.js
+++ src/homework/hw-lesson14/filterPeople/filterPeople.js
@@ -11,24 +11,28 @@
 
 /** Find doughters with parrent */
 function findDoughters(arr) {
-  // write code here
+  return arr.filter((item) => item.sex === 'f' && (item.father !== null || item.mother !== null))
 }
 
 /** Find people who lived over 60 years */
 function findAllPeopleWhoLivedOver60(arr) {
-  // write code here
+  return arr.filter((item) => (item.died - item.born) > 60)
 }
 
 /** Find people without both parrents */
 function findAllPeopleWithoutBothParrent(arr) {
-  // write code here
+  return arr.filter((item) => item.mother == null && item.father == null)
 }
 
 /** Find people with 3 children */
 function findPeopleWithTreeChildren(arr) {
-  // write code here
+  return arr.filter((item) => {
+  const children = arr.filter((child) => {
+  return child.father === item.name || child.mother === item.name;
+  })
+  return children.length === 3
+})
 }
-
 module.exports = {
   findDoughters,
   findAllPeopleWhoLivedOver60,
