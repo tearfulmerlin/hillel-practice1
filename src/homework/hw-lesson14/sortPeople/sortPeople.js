5461c4924c1d6358e72bb3ca037a1ddb92a218ee
Hladiy Taras <tarik2002sta@gmail/com>
2023-01-26 12:36:02 +0200

    mission complete

--- src/homework/hw-lesson14/sortPeople/sortPeople.js
+++ src/homework/hw-lesson14/sortPeople/sortPeople.js
@@ -11,17 +11,27 @@
 
 /** Sort by age */
 function sortByAge(arr) {
-  // write code here
+  let newArr = [...arr]
+  return newArr.sort((a, b) => {
+    const sortA = a.died - a.born;
+    const sortB = b.died - b.born
+    return sortA - sortB
+  })
 }
-
 /** Sort by name */
 function sortByName(arr) {
-  // write code here
+  let newArr = [...arr]
+  return newArr.sort((a,b) => a.name.localeCompare(b.name))
 }
 
 /** Sort by children quantity */
 function sortByChildrenQuantity(arr) {
-  // write code here
+  const newArray = [...arr];
+  return newArray.sort((a,b) => {
+    const onechild = arr.filter((item) => item.father === a.name || item.mother === a.name)
+    const moreChid = arr.filter((item) => item.father === b.name || item.mother === b.name)
+    return onechild.length - moreChid.length
+  })
 }
 
 module.exports = {
