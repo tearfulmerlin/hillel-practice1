3f45f42046bbc630b1e30f373bfd52c80c80f371
SOFIASEMCHUK <semchuk.s02@gmail.com>
2023-01-27 17:06:46 +0200

    test5

--- src/task5/isPalindrome.js
+++ src/task5/isPalindrome.js
@@ -19,7 +19,33 @@
  * @return {boolean}
  */
 function isPalindrome(str) {
-  // write code here
+  if (str === '') {
+    return true;
+  }
+  return function a() {
+    let arr = [];
+    let arr2 = [];
+    for (let i = 0; i < str.length / 2; i++){
+      
+      if (true) {
+        arr = arr + i;
+      }
+    }
+    for (let i = str[str.length - 1]; i < str.length / 2; i--) {
+      
+      if (true) {
+        arr2 = arr2 + i;
+      }
+    }
+    if (arr.toLowerCase() !== arr2.reverse().toLowerCase()) {
+      return false;
+    }
+  }
+  // str = str.toLowerCase();
+  // let reversed = str.reverse();
+  // if (str === reversed.reverse()) {
+  //   return true;
+  // }
 }
 
 module.exports = isPalindrome;
