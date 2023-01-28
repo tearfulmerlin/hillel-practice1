bc9ccc13f6a729f80dfc255bdb16242b865d5eee
Hladiy Taras <tarik2002sta@gmail/com>
2023-01-28 15:30:55 +0200

    task9

--- src/practice/task9/mapArr.js
+++ src/practice/task9/mapArr.js
@@ -7,6 +7,12 @@
  */
 
 
-function mapArr(arr, callback) {}
+function mapArr(arr, callback) {
+    const resalt = [];
+    for(let i = 0; i < arr.length;i++){
+        resalt[i] = callback(arr[i])
+    }
+    return resalt
+}
 
 module.exports = mapArr;
