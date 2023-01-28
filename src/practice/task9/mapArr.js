2ec6fdaa046a30f5dc18db55695d76ed6f958d0a
oleksii onyshchuk <oleksii.onyshchuk@additiv.com>
2023-01-28 12:42:37 +0200

    add task 9

---
+++ src/practice/task9/mapArr.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * @param {Array} arr
+ * @param {Function} callback
+  *
+ * @returns {Array}
+ */
+
+
+function mapArr(arr, callback) {}
+
+// const mapped = [1,2,3,4].map((value) => {
+//   return value * 2;
+// }) => [2, 4, 6, 8];
