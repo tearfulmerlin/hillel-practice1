572b5cb66ab0b66a37915da5c66f9ee6bc712bd6
Volodymyr Vyshniakov <wwwowka@gmail.com>
2023-01-28 12:57:49 +0200

    task9 (self-made method map)

--- src/practice/task9/mapArr.js
+++ src/practice/task9/mapArr.js
@@ -7,6 +7,16 @@
  */
 
 
-function mapArr(arr, callback) {}
+function mapArr(arr, callback) {
+  const result = [];
+
+  for (const element of arr) {
+    result.push(callback(element))
+  }
+  return result;
+}
+
+// const nums = [1, 2, 3, 4, 5, 6];
+// const maped = mapArr(nums, (item) => item * 2);
 
 module.exports = mapArr;
