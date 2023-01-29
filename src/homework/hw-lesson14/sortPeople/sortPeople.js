ea66e0c89c7b0cebb3ef5dd9dbce09eb34ede7a9
Dmitry Shumskiy <ejedima260@gmail.com>
2023-01-29 16:46:57 +0200

    sorting

--- src/homework/hw-lesson14/sortPeople/sortPeople.js
+++ src/homework/hw-lesson14/sortPeople/sortPeople.js
@@ -10,18 +10,53 @@
 */
 
 /** Sort by age */
+/**
+ * @param {Array} arr
+*/
 function sortByAge(arr) {
-  // write code here
+  const temp = [...arr];
+
+  return temp.sort((a, b) => {
+    const leftAge = a.died - a.born;
+    const rightAge = b.died - b.born;
+
+    return leftAge - rightAge;
+  });
 }
 
 /** Sort by name */
+/**
+ * @param {Array} arr
+*/
 function sortByName(arr) {
-  // write code here
+  const temp = [...arr];
+
+  // вточнити
 }
 
 /** Sort by children quantity */
+/**
+ * @param {Array} arr
+*/
 function sortByChildrenQuantity(arr) {
-  // write code here
+  const temp = [...arr];
+
+  temp.forEach((person) => {
+    const childrenAmnt = arr.filter((children) => {
+      const haveMother = children.mother === person.name;
+      const haveFather = children.father === person.name;
+
+      return haveMother || haveFather;
+    }).length;
+    person.childrenAmnt = childrenAmnt;
+  });
+
+  return temp.sort((a, b) => {
+    const leftChildrenAmnt = a.childrenAmnt;
+    const rightChildrenAmnt = b.childrenAmnt;
+
+    return leftChildrenAmnt - rightChildrenAmnt;
+  });
 }
 
 module.exports = {
