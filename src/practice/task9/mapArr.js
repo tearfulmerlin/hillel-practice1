4f18c2fcdc57b66e18f538963a348cc3d23608e8
Dmitry Shumskiy <ejedima260@gmail.com>
2023-02-01 12:50:54 +0200

    task9

--- src/practice/task9/mapArr.js
+++ src/practice/task9/mapArr.js
@@ -2,11 +2,18 @@
  *
  * @param {Array} arr
  * @param {Function} callback
-  *
+ *
  * @returns {Array}
  */
 
 
-function mapArr(arr, callback) {}
+function mapArr(arr, callback) {
+  const temp = [];
 
-module.exports = mapArr;
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i])) temp.push(callback(arr[i]));
+  }
+}
+const arr = [1, 2, 3, 4];
+console.log(mapArr(arr, (el) => el % 2 === 0));
+// module.exports = mapArr;
