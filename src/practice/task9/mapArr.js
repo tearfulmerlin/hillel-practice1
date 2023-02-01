ae84bd32123bf0c98844ff9da9b7666eb137e0e6
Dmitry Shumskiy <ejedima260@gmail.com>
2023-02-01 13:09:10 +0200

    array map

--- src/practice/task9/mapArr.js
+++ src/practice/task9/mapArr.js
@@ -11,9 +11,10 @@ function mapArr(arr, callback) {
   const temp = [];
 
   for (let i = 0; i < arr.length; i++) {
-    if (callback(arr[i])) temp.push(callback(arr[i]));
+    temp.push(callback(arr[i]));
   }
+
+  return temp;
 }
-const arr = [1, 2, 3, 4];
-console.log(mapArr(arr, (el) => el % 2 === 0));
-// module.exports = mapArr;
+
+module.exports = mapArr;
