53a04ed0dae4ef343c477d05a6b64f7c154da173
Dmitry Shumskiy <ejedima260@gmail.com>
2023-02-04 12:57:28 +0200

    table

--- src/homework/hw-lesson-18/index.js
+++ src/homework/hw-lesson-18/index.js
@@ -16,3 +16,20 @@ function onBlur(e) {
 const input = document.querySelector('#nameInput');
 input.onfocus = onFocus;
 input.onblur = onBlur;
+
+
+const rowAmnt = 10;
+const colAmnt = 10;
+let iterator = 1;
+const table = document.createElement('table');
+
+for (let i = 0; i < rowAmnt; i++) {
+    const tr = document.createElement('tr');
+    for (let j = 0; j < colAmnt; j++) {
+        const td = document.createElement('td');
+        td.innerText = iterator++;
+        tr.insertAdjacentElement('beforeend', td);
+    }
+    table.insertAdjacentElement('beforeend', tr);
+}
+document.querySelector('.table').insertAdjacentElement('beforeend', table);
