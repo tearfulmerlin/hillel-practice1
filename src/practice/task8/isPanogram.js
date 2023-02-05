0d84b03efe69314b30e2bad13a27e6b0695c84c3
SOFIASEMCHUK <semchuk.s02@gmail.com>
2023-02-05 18:56:01 +0200

    task8 - panogram

--- src/practice/task8/isPanogram.js
+++ src/practice/task8/isPanogram.js
@@ -5,9 +5,19 @@
  *
  * @returns {Boolean}
  */
-
 function isPangram(str) {
-  /* write code here */
-}
+  if (typeof str !== 'string' || str === undefined) {
+    return false;
+  }
 
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (!str.toLowerCase().includes(alphabet[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+// 
 module.exports = isPangram;
