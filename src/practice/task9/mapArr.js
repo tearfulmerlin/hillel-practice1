bd59b7dc8ace2fb7876a0f521668d6ef2f2a5b10
denys-lo <div.p.div@gmail.com>
2023-02-09 16:22:26 +0100

    HW20. Own map

--- src/practice/task9/mapArr.js
+++ src/practice/task9/mapArr.js
@@ -7,6 +7,13 @@
  */
 
 
-function mapArr(arr, callback) {}
+function mapArr(arr, callback) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(callback(arr[i]));
+  }
+
+  return result;
+}
 
 module.exports = mapArr;
