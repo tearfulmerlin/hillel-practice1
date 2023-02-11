af6d83a72fd2362f91d45dbafd8375e72d77b5a2
Volodymyr Vyshniakov <wwwowka@gmail.com>
2023-02-11 21:18:06 +0200

    hw22

--- src/homework/hw-lesson20/index.js
+++ src/homework/hw-lesson20/index.js
@@ -11,3 +11,29 @@
  *
  * При кілку на кнопку "save", вивести таблицю з даними під формою
  */
+
+const form = document.getElementById('regForm');
+function printData(arr) {
+  const dataTabel = document.createElement('div');
+  form.after(dataTabel);
+  for (let i = 0; i < arr.length; i++) {
+    const cell = document.createElement('div');
+    // eslint-disable-next-line prefer-destructuring
+    cell.innerText = `${arr[i][0]} : ${arr[i][1]}`;
+    dataTabel.appendChild(cell);
+  }
+}
+
+function printLabel(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i].innerText);
+  }
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  printData([...new FormData(form).entries()]);
+  printLabel(document.querySelectorAll('label'));
+});
+
+console.log([...new FormData(form).entries()]);
