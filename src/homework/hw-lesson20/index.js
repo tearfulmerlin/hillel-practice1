ff2994dc8ad9a01755516df1f3ecfa754b6a0d78
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2023-02-12 19:50:32 +0200

    hw22-kovalchuk

--- src/homework/hw-lesson20/index.js
+++ src/homework/hw-lesson20/index.js
@@ -11,3 +11,12 @@
  *
  * При кілку на кнопку "save", вивести таблицю з даними під формою
  */
+
+
+window.onload = () => {
+    const popUp = window.open("file:///C:/Users/glebk/OneDrive/Рабочий%20стол/kovalchuk/hillel-practice1/src/homework/hw-lesson20/popup.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=800,right=800,width=800,height=800");
+      
+}
+
+const body = document.querySelector('body');
+body.style.cssText = 'background-color: black;';
