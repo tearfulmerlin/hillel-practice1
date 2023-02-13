ebb5d4ceaba20ac4e3f18262f7ac14a6ef8f85bb
Dmitry Shumskiy <ejedima260@gmail.com>
2023-02-13 11:37:43 +0200

    hw-23

--- src/homework/hw-lesson-19/index.js
+++ src/homework/hw-lesson-19/index.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line import/extensions
+// eslint-disable import/extensions
 import assortment from './assortment.js';
 
 /**
@@ -11,3 +11,52 @@ import assortment from './assortment.js';
  * і повернення в початковий стан (коли відображється тільки список категорій)
  *
  */
+
+function removeActiveClasses(parent) {
+  for (const el of parent.children) {
+    el.classList.remove('active');
+  }
+}
+
+function clearAllNeighbours(col) {
+  if (col !== null) {
+    const div = col.querySelector('div');
+    div.innerHTML = '';
+    clearAllNeighbours(col.nextElementSibling);
+  }
+}
+
+function renderColumn(col, list) {
+  if (col !== null) {
+    const categoryBlock = col.querySelector('div');
+    categoryBlock.innerHTML = '';
+    for (const el of list) {
+      const node = document.createElement('div');
+
+      node.innerText = el.title.toUpperCase();
+      categoryBlock.appendChild(node);
+
+      node.addEventListener('click', function () {
+        removeActiveClasses(this.parentNode);
+        clearAllNeighbours(this.parentElement.parentElement.nextElementSibling);
+
+        this.classList.add('active');
+        const assort = list.find((product) => product.title === this.textContent.toLowerCase());
+        renderColumn(this.parentElement.parentElement.nextElementSibling, assort.children);
+      });
+    }
+
+    const models = col.querySelector('.model');
+    if (models !== null) {
+      for (const product of models.children) {
+        product.addEventListener('click', function() {
+          const marks = document.querySelector('.trade-mark .active');
+          alert(`You bought ${marks.textContent} ${this.textContent}!`);
+          clearAllNeighbours(document.querySelector('.col').nextElementSibling);  
+        });
+      }
+    }
+  }
+}
+
+renderColumn(document.querySelector('.col'), assortment);
