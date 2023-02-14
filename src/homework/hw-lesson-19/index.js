56dae4d697abdd3eddc75c767cedf3807d0f828c
oleksii onyshchuk <oleksii.onyshchuk@additiv.com>
2023-02-14 17:25:53 +0200

    hw23 solution

--- src/homework/hw-lesson-19/index.js
+++ src/homework/hw-lesson-19/index.js
@@ -11,3 +11,62 @@ import assortment from './assortment.js';
  * і повернення в початковий стан (коли відображється тільки список категорій)
  *
  */
+
+const cart = [];
+
+window.showCart = () => {console.log({cart})}
+
+const productContainer = document.querySelector('.products');
+const markContainer = document.querySelector('.trade-mark');
+const modelContainer = document.querySelector('.model');
+
+function renderModels(modelList) {
+  modelList.forEach((item) => {
+    const model = document.createElement('div');
+
+    model.innerText = item.title;
+    model.style.cursor = 'pointer';
+    model.addEventListener('click', () => {
+      cart.push(item);
+      const message = document.createElement('div');
+      message.innerText = `${item.title} has been added`;
+      document.body.appendChild(message);
+      setTimeout(() => {
+        message.innerHTML = '';
+      }, 2000);
+    });
+    modelContainer.appendChild(model);
+  });
+}
+
+function renderTradeMarks(tradeMarkList) {
+  tradeMarkList.forEach((item) => {
+    const product = document.createElement('div');
+
+    product.innerText = item.title;
+    product.style.cursor = 'pointer';
+    product.addEventListener('click', () => {
+      modelContainer.innerHTML = '';
+      renderModels(item.children);
+    });
+    markContainer.appendChild(product);
+  });
+}
+
+function renderCategories(productList) {
+  productList.forEach((item) => {
+    const product = document.createElement('div');
+    product.innerText = item.title;
+    product.style.cursor = 'pointer';
+
+    product.addEventListener('click', () => {
+      markContainer.innerHTML = '';
+      modelContainer.innerHTML = '';
+      renderTradeMarks(item.children);
+    });
+
+    productContainer.appendChild(product);
+  });
+}
+
+renderCategories(assortment);
