5abb1e84e833e1543d6a18f5cfcb2834370e3766
SOFIASEMCHUK <semchuk.s02@gmail.com>
2023-02-15 17:34:55 +0200

    hw18

--- src/homework/hw-lesson-18/index.js
+++ src/homework/hw-lesson-18/index.js
@@ -15,11 +15,11 @@ const input = document.querySelector('input');
 span.style.color = "red";
 
 function clickInput() {
-    divClass.appendChild(span);
+  divClass.appendChild(span);
 }
 
 function bluring() {
-    divClass.removeChild(span);
+  divClass.removeChild(span);
 }
 
 input.onclick = clickInput;
@@ -31,6 +31,33 @@ input.onblur = bluring;
  * Table should generate dynamicly.
  */
 
+const tableParent = document.querySelector('.table');
+const br2 = document.createElement('br');
+tableParent.appendChild(br2);
+const table = document.createElement('table');
+tableParent.appendChild(table);
+table.style.border = "1px solid red";
+table.style.width = "200px";
+table.style.height = "200px";
+table.style.margin = "0 auto";
+
+table.classList.add('table-class');
+let num = 1;
+
+function tabled() {
+  for (let i = 0; i < 10; i++) {
+    const tr = document.createElement('tr');
+    for (let j = 0; j < 10; j++) {
+      const td = document.createElement('td');
+      tr.appendChild(td);
+      td.style.border = '1px solid red'
+      td.innerText = num++;
+    }
+    table.appendChild(tr);
+  }
+}
+tabled();
+
 /**
  * Task 3 (button-links)
  * Create 2 buttons on the page with js
@@ -41,9 +68,43 @@ input.onblur = bluring;
  * show message: "Please click on the first button"
  */
 
+const buttonLinks = document.querySelector('.button-links');
+const button1 = document.createElement('button');
+const button2 = document.createElement('button');
+buttonLinks.appendChild(button1);
+buttonLinks.appendChild(button2);
+
+button1.innerText = 'button1';
+button1.style.color = 'yellow';
+button1.style.backgroundColor = 'blue';
+button2.innerText = 'button2';
+button2.style.color = 'blue';
+button2.style.backgroundColor = 'yellow';
+
+let url = '';
+button1.addEventListener('click', () => {
+  url = prompt('Enter your URL');
+});
+
+button2.addEventListener('click', () => {
+  if (url) {
+    if (!url.startsWith('https') || !url.startsWith('http')) {
+      url = `https://${url}`;
+    }
+    window.location.replace(url);
+  } else {
+    alert('Enter url in button1');
+  }
+});
 
 /**
  * Task 4 (random-pics)
  * Display pictures from "images" folder in a random order
  * (using Math.random)
  */
+
+const picture = document.querySelector('.random-pics');
+const image = document.createElement('img');
+picture.appendChild(image);
+const random = Math.floor(Math.random() * 10);
+image.setAttribute('src', `images/${random}.jpg`);
