1d944fcb0e4aa73ab73d3393d65b3107aa14870d
morqua <mariasersneva@gmail.com>
2023-02-17 00:26:31 +0200

    homewrok!!!!

--- src/homework/hw-lesson14/sortPeople/sortPeople.js
+++ src/homework/hw-lesson14/sortPeople/sortPeople.js
@@ -11,17 +11,43 @@
 
 /** Sort by age */
 function sortByAge(arr) {
-  // write code here
+  return [...arr].sort((a, b) => {
+    const ageA = a.died - a.born;
+    const ageB = b.died - b.born;
+
+    return ageA - ageB;
+  });
 }
 
 /** Sort by name */
 function sortByName(arr) {
-  // write code here
+  function sorting(a, b) {
+    const nameA = a.name;
+    const nameB = b.name;
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    return 0;
+  }
+
+  return [...arr].sort(sorting);
 }
 
 /** Sort by children quantity */
 function sortByChildrenQuantity(arr) {
-  // write code here
+  const parents = arr.filter((parent) => {
+    const children = arr.filter((child) => parent.name === child.father
+    || parent.name === child.mother);
+    console.log(children.sort((a, b) => a.length - b.length));
+
+    return children.sort((a, b) => b.length - a.length);
+  });
+
+  return parents;
 }
 
 module.exports = {
