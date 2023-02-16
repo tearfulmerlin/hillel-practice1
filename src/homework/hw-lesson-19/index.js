97863636a0f5ef5e8d65a32e5b49244af8bbf487
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2023-02-16 14:48:27 +0200

    hw23-kovalchuk

--- src/homework/hw-lesson-19/index.js
+++ src/homework/hw-lesson-19/index.js
@@ -11,3 +11,58 @@ import assortment from './assortment.js';
  * і повернення в початковий стан (коли відображється тільки список категорій)
  *
  */
+
+function getTable(col, list) {
+  const category = col.querySelector('div');
+  category.innerHTML = '';
+
+  for (const e of list) {
+    const div = document.createElement('div');
+
+    div.innerText = e.title.toUpperCase();
+    category.appendChild(div);
+
+    div.onclick = function () {
+      deleteClasses(this.parentNode);
+      getClear(this.parentElement.parentElement.nextElementSibling);
+
+      this.classList.add('active');
+
+      const select = list.find((product) => product.title === this.textContent.toLowerCase());
+      getTable(this.parentElement.parentElement.nextElementSibling, select.children);
+    }
+  }
+
+  const model = col.querySelector('.model');
+
+  for (const product of model.children) {
+    product.onclick = function () {
+
+      const mark = document.querySelector('.trade-mark .active');
+      const resultBuy = document.querySelector('.prod');
+      resultBuy.innerHTML = `You buy: ${mark.textContent} ${this.textContent}!`;
+
+      getClear(document.querySelector('.col').nextElementSibling);
+      deleteClasses(document.querySelector('.products'));
+    }
+  }
+}
+
+function deleteClasses(parent) {
+  for (const e of parent.children) {
+    e.classList.remove('active');
+  }
+}
+
+function getClear(col) {
+  if (col !== null) {
+    const div = col.querySelector('div').innerHTML = '';
+
+    getClear(col.nextElementSibling);
+  }
+}
+
+const removeRes = () => document.getElementById('res').remove();
+document.querySelector('.delete').addEventListener('click', removeRes);
+
+getTable(document.querySelector('.col'), assortment);
