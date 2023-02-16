2a93d0f9c8236167db9c2b2b2b2c47dcec0362f0
Dmitry Shumskiy <ejedima260@gmail.com>
2023-02-16 23:27:31 +0200

    more animation\style upgrades

--- src/homework/hw-lesson21/script.js
+++ src/homework/hw-lesson21/script.js
@@ -13,19 +13,24 @@ function showError() {
     input.classList.remove('error');
   }, 3000);
 }
+
 function deleteAllSelected() {
   document.querySelectorAll('.control-panel button').forEach((el) => {
     el.classList.remove('selected');
   });
 }
+
 function getRndNum() {
   const date = new Date();
   const str = (date.getDay() + date.getMinutes() + date.getMilliseconds()).toString();
 
   return Math.random(99999).toString() + str;
 }
+
 function calculateProgress() {
   const slider = document.querySelector('.slider');
+  const counter = document.querySelector('.slider-counter');
+  const deleteCompleteBtn = document.querySelector('#deleteCompl');
   const amntComplete = list.reduce((accum, el) => {
     if (el.complete) {
       accum += 1;
@@ -33,35 +38,40 @@ function calculateProgress() {
 
     return accum;
   }, 0);
-  const counter = document.querySelector('.slider-counter');
 
-  const deleteCompleteBtn = document.querySelector('#deleteCompl');
   if (amntComplete) {
     deleteCompleteBtn.classList.remove('disable');
   } else {
     deleteCompleteBtn.classList.add('disable');
   }
-  counter.innerText = `${amntComplete}/${list.length}`;
 
+  counter.innerText = `${amntComplete}/${list.length}`;
   slider.style.width = `${list.length ? (amntComplete / list.length) * 100 : 0}%`;
 }
+
 function taskClick(e) {
   const elementName = e.target.nodeName;
   if (elementName !== 'LI') {
     const id = e.target.parentElement.getAttribute('data-id');
-    const index = list.findIndex((el) => el.id === id);
+    const arrayIndex = list.findIndex((el) => el.id === id);
 
     if (elementName === 'LABEL') {
-      list[index].complete = !list[index].complete;
-      calculateProgress();
+      list[arrayIndex].complete = !list[arrayIndex].complete;
     }
     if (elementName === 'BUTTON') {
-      list.splice(index, 1);
-      renderTasks(list);
-      taskList.style.maxHeight = `${taskList.scrollHeight}px`;
+      list.splice(arrayIndex, 1);
+      const currEl = e.target.parentElement;
+      currEl.classList.add('hide');
+
+      currEl.ontransitionend = () => {
+        currEl.remove();
+        taskList.style.maxHeight = '100%';
+      };
     }
+    calculateProgress();
   }
 }
+
 function renderTasks(arr) {
   calculateProgress();
 
@@ -79,14 +89,30 @@ function renderTasks(arr) {
     taskList.insertAdjacentElement('beforeend', li);
   }
 }
+
+function appendTask(elem) {
+  const li = document.createElement('li');
+  const template = `
+      <input type="checkbox" name="taskCheck" id="${elem.id}" ${elem.complete ? 'checked' : ''}>
+      <label for="${elem.id}">${elem.text}</label>
+      <button class="trash"></button>`;
+
+  li.setAttribute('data-id', elem.id);
+  li.insertAdjacentHTML('beforeend', template);
+  li.addEventListener('click', taskClick);
+  taskList.insertAdjacentElement('beforeend', li);
+}
+
 function addTask(e) {
   if (e.target.nodeName === 'BUTTON' || e.key === 'Enter') {
+    //! if input is empty
     if (input.value.trim() === '') {
       showError();
       input.value = '';
 
       return 0;
     }
+    //! object
     const temp = {
       text: input.value.replace('\n', ''),
       complete: false,
@@ -96,7 +122,7 @@ function addTask(e) {
     list.push(temp);
     deleteAllSelected();
     document.querySelector('#showAll').classList.add('selected');
-    renderTasks(list);
+    appendTask(temp);
     taskList.style.maxHeight = `${taskList.scrollHeight}px`;
   }
 
