ce268e8272a9d169f003c589305e8f6aaef44290
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2023-02-17 15:41:08 +0200

    hw24-kovalchuk

--- src/lessons/lesson21/index.js
+++ src/lessons/lesson21/index.js
@@ -1,72 +1,85 @@
-let store = [
-  { title: 'todo', id: 1, done: true },
-  { title: 'remove', id: 2, done: false },
-];
 
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
-const input = document.querySelector('input');
-const list = document.querySelector('ul');
+let store = [];
 
-function renderItems(data) {
-  list.innerHTML = '';
+function showCounter() {
+    const uncompleted = store.filter((item) => !item.classList.contains('completed')).length;
+    const counter = document.querySelector('.count');
+    counter.textContent = `${uncompleted} item left`;
+}
 
-  data.forEach((item) => {
-    list.innerHTML += `
-      <li class="${item.done ? 'done' : ''}">
-        ${item.title}
-        <button id=${item.id}>delete</button>
-      </li>
+todoButton.addEventListener('click', function addTodo(e) {
+
+    e.preventDefault();
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    todoDiv.appendChild(newTodo);
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-check-fill" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zm8.854-9.646a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
+    </svg>
     `;
-  });
-}
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
 
-// intial load
-renderItems(store);
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+    </svg>
+    `;
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
 
-function generateId() {
-  return Math.random(1000) + (new Date().getTime());
-}
+    todoList.appendChild(todoDiv);
 
-input.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    const item = { title: event.target.value, id: generateId() };
-
-    event.target.value = '';
-    store.push(item);
-    renderItems(store);
-  }
+    todoInput.value = '';
 });
 
-list.addEventListener('click', (event) => {
-  if (event.target.tagName === 'BUTTON') {
-    store = store.filter((item) => item.id !== +event.target.id);
-    renderItems(store);
-  } else if (event.target.tagName === 'LI') {
-    store = store.map((item) => (item.id === +event.target.firstElementChild.id
-      ? {
-        ...item,
-        done: !item.done,
-      }
-      : item));
+todoList.addEventListener('click', function deleteCheck(e) {
+    const item = e.target;
+    const todo = item.parentElement;
 
-    renderItems(store);
-  }
+    if (item.classList[0] === 'trash-btn') todo.classList.add('fall');
+    todo.addEventListener('transitioned', function () {
+        todo.remove();
+    })
+    if (item.classList[0] === 'complete-btn') todo.classList.toggle('completed');
 });
 
-document.querySelector('#controls').addEventListener('click', (event) => {
-  if (event.target.id === 'all') {
-    renderItems(store);
-  }
-  if (event.target.id === 'active') {
-    const filtred = store.filter((item) => !item.done);
-    renderItems(filtred);
-  }
-  if (event.target.id === 'completed') {
-    const filtred = store.filter((item) => item.done);
-    renderItems(filtred);
-  }
-  if (event.target.id === 'clear') {
-    store = store.filter((item) => !item.done);
-    renderItems(store);
-  }
+filterOption.addEventListener('click', function getFilter(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function (todo) {
+        switch (e.target.value) {
+            case 'all':
+                todo.style.display;
+                break;
+            case 'completed':
+                (todo.classList.contains('completed'))
+                    ? todo.style.display = 'flex'
+                    : todo.style.display = 'none';
+                break;
+            case 'uncompleted':
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                    store.push(todo);
+                    showCounter(todo);
+                } else {
+                    todo.style.display = 'none';
+                    store.push(todo);
+                    showCounter(todo);
+                }
+        }
+    })
 });
+
