ae45aaa842f94a62d23b10269b0862c8058cf8cd
oleksii onyshchuk <oleksii.onyshchuk@additiv.com>
2023-02-19 01:00:49 +0200

    add homework for lesson 22

---
+++ src/homework/hw-lesson22/customFilter.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * Create customFilter method which behave same way as Array.filter method
+ * The customFilter method will be assigned to Array.prototype
+ *
+ * @param {Function} callback
+ *
+ * @returns {Array}
+ */
+
+function customFilter(callback) {
+  /* write code here */
+}
+
+module.exports = customFilter;
