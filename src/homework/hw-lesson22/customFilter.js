2729e75a0ae1a932d613d9675a86fb06c4cd48d4
sergey-salo <ssnonmail@gmail.com>
2023-02-20 19:37:51 +0200

    added functionality of hw25

--- src/homework/hw-lesson22/customFilter.js
+++ src/homework/hw-lesson22/customFilter.js
@@ -7,9 +7,18 @@
  *
  * @returns {Array}
  */
-
 function customFilter(callback) {
-  /* write code here */
+  const result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    const item = this[i];
+
+    if (callback(item, i, this)) {
+      result.push(item);
+    }
+  }
+
+  return result;
 }
 
 module.exports = customFilter;
