586b42810e618cd935218fec32fd4d6a12a52cca
sergey-salo <ssnonmail@gmail.com>
2023-02-20 09:45:15 +0200

    added functionality of hw23

--- src/homework/hw-lesson-19/index.js
+++ src/homework/hw-lesson-19/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-restricted-syntax */
 // eslint-disable-next-line import/extensions
 import assortment from './assortment.js';
 
@@ -11,4 +14,122 @@ import assortment from './assortment.js';
  * і повернення в початковий стан (коли відображється тільки список категорій)
  *
  */
+const columns = [
+  document.querySelector('.products'),
+  document.querySelector('.trade-mark'),
+  document.querySelector('.model'),
+];
 
+function addClass(element, className) {
+  element.classList.add(className);
+}
+
+function deleteClassOnElements(element, className) {
+  const liElements = element.parentNode.querySelectorAll('li');
+
+  for (const liElement of liElements) {
+    if (liElement.classList.contains(className)) {
+      liElement.classList.remove(className);
+      break;
+    }
+  }
+}
+
+function cleanChildrenDisplay(type, title) {
+  const activeCategory = assortment.find((x) => x.isActive);
+  if (!activeCategory) return;
+
+  if (type === undefined) {
+    if (activeCategory.title === title) {
+      activeCategory?.children.forEach((tradeMarkItem) => {
+        tradeMarkItem.isActive = false;
+        tradeMarkItem.children.forEach((modelItem) => { modelItem.isSelected = false; });
+      });
+    } else {
+      activeCategory.isActive = false;
+      activeCategory?.children.forEach((tradeMarkItem) => { tradeMarkItem.isActive = false; });
+    }
+  }
+
+  if (type === 'trade mark') {
+    const activeTradeMark = activeCategory.children.find((x) => x.isActive);
+    if (!activeTradeMark) return;
+
+    activeTradeMark.isActive = false;
+  }
+
+  if (type === 'model') {
+    const activeTradeMark = activeCategory.children.find((x) => x.isActive);
+    const activeModels = activeTradeMark.children
+      .filter((item) => item.isSelected && item.title === title);
+    activeModels?.forEach((item) => { item.isSelected = false; });
+  }
+}
+
+function cleanNextSeblingElementemnt(element) {
+  if (element.nextElementSibling) {
+    cleanNextSeblingElementemnt(element.nextElementSibling);
+  }
+
+  const div = element.querySelector('div');
+  div.innerHTML = '';
+}
+
+function cleanElementsWithChildren(element) {
+  const parent = element.closest('.col');
+
+  if (parent?.nextElementSibling) {
+    cleanNextSeblingElementemnt(parent.nextElementSibling);
+  }
+}
+
+function render(data, columnIndex) {
+  const ul = document.createElement('ul');
+
+  data.forEach((item) => {
+    const li = document.createElement('li');
+    li.textContent = item.title;
+
+    if (Array.isArray(item?.children)) {
+      li.addEventListener('click', (e) => {
+        const processingItem = item;
+        const { isActive } = item;
+
+        cleanElementsWithChildren(e.target);
+        deleteClassOnElements(e.target, 'active');
+
+        if (!processingItem.isActive) {
+          addClass(e.target, 'active');
+          render(item.children, columnIndex + 1);
+        }
+
+        cleanChildrenDisplay(processingItem.type, item.title);
+        processingItem.isActive = !isActive;
+      });
+    } else {
+      item.isSelected && li.classList.add('selected');
+
+      li.addEventListener('click', (e) => {
+        const processingItem = item;
+        const { isSelected } = item;
+
+        if (processingItem.isSelected) {
+          deleteClassOnElements(e.target, 'selected');
+          cleanChildrenDisplay(processingItem.type, item.title);
+        } else {
+          addClass(e.target, 'selected');
+          alert(`"${e.target.innerText}" is purchased`);
+        }
+
+        processingItem.isSelected = !isSelected;
+      });
+    }
+
+    ul.append(li);
+  });
+
+  columns[columnIndex].innerHTML = '';
+  columns[columnIndex].append(ul);
+}
+
+render(assortment, 0);
