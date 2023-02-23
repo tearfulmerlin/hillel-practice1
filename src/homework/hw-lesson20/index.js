a3650cc1b1b7aea5edb27f2213b14ea8d42aa8d2
Hladiy Taras <tarik2002sta@gmail/com>
2023-02-23 14:24:33 +0200

    done

--- src/homework/hw-lesson20/index.js
+++ src/homework/hw-lesson20/index.js
@@ -11,3 +11,24 @@
  *
  * При кліку на кнопку "save", вивести таблицю з даними під формою
  */
+const forms = document.forms;
+const submit = document.querySelector('button.submit');
+
+submit.addEventListener('click', (event) => {
+  event.preventDefault();
+  const newForm = [...new FormData(forms['myForm'])];
+  const table = document.createElement('table');
+  table.setAttribute('border', '1');
+  newForm.forEach((item) => {
+    const td = document.createElement('td');
+    const th = document.createElement('th');
+    const tr = document.createElement('tr');
+    th.style.minWidth = '100px';
+    td.innerText = item[0];
+    th.innerText = item[1];
+    table.appendChild(tr);
+    tr.appendChild(td);
+    tr.appendChild(th);
+    document.body.appendChild(table);
+  });
+});
