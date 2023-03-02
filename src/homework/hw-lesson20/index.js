16f914c536f6eb832bdae60e411c87b7092c7938
SOFIASEMCHUK <semchuk.s02@gmail.com>
2023-03-02 15:03:02 +0200

    hw-22

--- src/homework/hw-lesson20/index.js
+++ src/homework/hw-lesson20/index.js
@@ -9,5 +9,42 @@
  * Languages (en, ru, ua ...)
  * Button "save"
  *
- * При кkіку на кнопку "save", вивести таблицю з даними під формою
+ * При кліку на кнопку "save", вивести таблицю з даними під формою
  */
+
+const forms = document.forms.formPeople;
+
+const formPersone = document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const eachElement = [...new FormData(forms).entries()].filter((item) => item[1] !== '');
+
+  const table = document.createElement('div');
+  const col1 = document.createElement('div');
+  const col2 = document.createElement('div');
+  document.body.appendChild(table);
+  table.style.cssText = `
+    display: flex;
+    margin-top: 5px;
+  `;
+  table.appendChild(col1);
+  table.appendChild(col2);
+
+  eachElement.forEach((item) => {
+    const col1Items = document.createElement('div');
+    col1Items.innerText = item[0] + ':';
+    col1.appendChild(col1Items);
+    col1Items.style.cssText = `
+      border: 1px solid red;
+      padding: 3px;
+      font-weight: 600;
+    `;
+
+    const col2Items = document.createElement('div');
+    col2Items.innerText = item[1];
+    col2.appendChild(col2Items);
+    col2Items.style.cssText = `
+      border: 1px solid red;
+      padding: 3px; 
+    `;
+  });
+});
