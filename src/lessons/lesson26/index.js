d56cdf01d2db8c6c85a810339ee79a99da92b10e
oleksii onyshchuk <oleksii.onyshchuk@additiv.com>
2023-03-05 13:04:03 +0200

    add files

---
+++ src/lessons/lesson26/index.js
@@ -0,0 +1,64 @@
+// const user = {
+//   name: '',
+//   skills: [
+//     // {
+//       // name: '',
+//     // }
+//   ],
+
+// }
+
+// user.skills?.[0]?.name;
+
+// const arr = [new Promise(), new Promise(), new Promise()];
+
+// for (const res of await Promise.all(arr)) {
+// }
+
+
+
+/** LIBRARIES */
+// JQuery + pugins
+// moment.js
+// 03/12/2000
+// 12.03.2000
+// 12.03.22 + 50
+// 12-03-2000
+// charts.js, D3, recharts
+// underscore / lodash
+// bootstrap, tailwindcss
+// slick
+// ag-grid
+// react.js, redux, material-ui, antd, rc
+// react-native
+// rx-js
+
+
+
+/** Server Side Rendering vs Client Side Rendering */
+/** SSR vs CSR */
+
+/** FRAMEWORKS
+ * whole set of instruments for website development
+ *
+ * data binding
+ * rounting
+ * project structure
+ * naming conventions
+ *
+ * SPA
+ * PWA
+*/
+
+// angularjs, angular
+// react - ecosystem // remix, nextjs, svelte
+// vue.js
+// backbone
+// EmberJS
+
+// distribution
+// of.website
+// CDN
+// NPM
+
+
