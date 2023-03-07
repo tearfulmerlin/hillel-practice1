fde1ffd33265e2e0e461bad29a713ce9ad3bbfa3
Volodymyr Vyshniakov <wwwowka@gmail.com>
2023-03-07 17:22:11 +0200

    almost works

--- src/homework/hw-lesson24/index.js
+++ src/homework/hw-lesson24/index.js
@@ -1,43 +1,65 @@
 const output = document.getElementById('output');
-
 const input = document.getElementById('input');
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const resetBtn = document.getElementById('reset');
+
 let refreshInterval = null;
 let seconds = '500';
-let arrInp = ['0', '0', 5];
+let arrSeconds = ['0', '0', 5];
 let setSeconds = seconds.toString();
 
-function rendering() {
-  const countdonwn = `${(arrInp[3] ? arrInp[3] : '  ') + (arrInp[2] ? arrInp[2] : '  ')}m ${(arrInp[1] ? arrInp[1] : '  ') + (arrInp[0] ? arrInp[0] : '  ')}s`
+function converted() {
+  if (arrSeconds[1] > 5) {
+    arrSeconds[1] -= 6;
+    arrSeconds[2] ?? (arrSeconds[2] = 0);
+    arrSeconds[2] = +arrSeconds[2] + 1;
+  }
+}
+
+function renderingCountdown() {
+  converted();
+  const countdonwn = `${(arrSeconds[3] ?? '  ') + (arrSeconds[2] ?? '  ')}m ${(arrSeconds[1] ?? '  ') + (arrSeconds[0] ?? '  ')}s`;
   input.value = seconds;
   document.title = countdonwn;
   output.innerText = countdonwn;
 }
-rendering();
+renderingCountdown();
 
 function updateArray(e) {
   setSeconds = e.target.value;
   seconds = setSeconds;
-  arrInp = seconds.split('').reverse();
-  rendering();
+  arrSeconds = seconds.split('').reverse();
 }
 
-input.addEventListener('input', updateArray);
-
-function start() {
+function startCountdown() {
   refreshInterval = setInterval(() => {
     seconds -= 1;
-    seconds = `${seconds}`;
-    arrInp = seconds.split('').reverse();
-    rendering();
+    arrSeconds = seconds.toString().split('').reverse();
+    renderingCountdown();
+    startBtn.style.display = 'none';
+    stopBtn.style.display = 'inline';
   }, 1000);
 }
 
-function stop() {
+function stopCountdown() {
   clearInterval(refreshInterval);
+  startBtn.style.display = 'inline';
+  stopBtn.style.display = 'none';
 }
 
-function reset() {
+function resetCountdown() {
   seconds = setSeconds;
-  stop();
-  rendering();
+  arrSeconds = seconds.split('').reverse();
+  stopCountdown();
+  renderingCountdown();
 }
+
+input.addEventListener('input', updateArray);
+input.addEventListener('focusout', renderingCountdown);
+input.addEventListener('keyup', ({ key }) => {
+  if (key === 'Enter') startCountdown();
+});
+startBtn.addEventListener('click', startCountdown);
+stopBtn.addEventListener('click', stopCountdown);
+resetBtn.addEventListener('click', resetCountdown);
