029cea9b98d4acddc3a6e4be1b85de500661f7d0
Dmitry Shumskiy <ejedima260@gmail.com>
2023-03-08 16:33:47 +0200

    message

---
+++ src/homework/hw-lesson23/burgers/index.js
@@ -0,0 +1,18 @@
+class Hamburgers {
+
+  constructor (SIZE) {
+    this.size = SIZE;
+  }
+
+  set SIZE_SMALL {
+    this.size = 'small';
+  }
+
+  set SIZE_BIG {
+    this.size = 'big';
+  }
+}
+
+
+
+
