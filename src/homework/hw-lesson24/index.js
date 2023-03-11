94878b54127ba929a04e948c63e5ea12cf03d547
Volodymyr Vyshniakov <wwwowka@gmail.com>
2023-03-11 20:30:21 +0200

    hw_Vyshniakov

--- src/homework/hw-lesson24/index.js
+++ src/homework/hw-lesson24/index.js
@@ -1,16 +1,21 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-use-before-define */
 const output = document.getElementById('output');
 const input = document.getElementById('input');
 const startBtn = document.getElementById('start');
 const stopBtn = document.getElementById('stop');
 const resetBtn = document.getElementById('reset');
 const cursorElement = document.createElement('span');
+const progressBar = document.getElementById('progress-bar');
 cursorElement.innerText = 's';
 
 let refreshInterval = null;
+let setSeconds = 0;
+let seconds = input.value;
 
 // 598 -> 6m 38s; 128 -> 1m 28s
-const convStartTime = (seconds) => {
-  const arrSeconds = [...seconds.toString().split('').reverse()];
+const convStartTime = (second) => {
+  const arrSeconds = [...second.toString().split('').reverse()];
   if (arrSeconds[1] > 5) {
     arrSeconds[1] -= 6;
     arrSeconds[2] ?? (arrSeconds[2] = 0);
@@ -26,22 +31,27 @@ function setSec(arr) {
 }
 
 
-function render(seconds, divider) {
-  const minutes = Math.floor(seconds / divider);
-  let remainingSeconds = seconds % divider;
+function render(second, divider) {
+  const minutes = Math.floor(second / divider);
+  let remainingSeconds = second % divider;
   if (remainingSeconds < 10) {
     remainingSeconds = `0${remainingSeconds}`;
   }
 
   output.innerText = `${minutes}m ${remainingSeconds}`;
   output.appendChild(cursorElement);
+
+  document.title = `${minutes}m ${remainingSeconds}s`;
+
+  const progress = (1 - (second / setSec(convStartTime(input.value)))) * 100;
+  progressBar.style.width = `${progress}%`;
 }
 
 render(setSec(convStartTime(input.value)), 60);
-let setSeconds = 0;
-let seconds = input.value;
+progressBar.style.width = '0%';
 
 function startCountdown() {
+  progressBar.style.backgroundColor = '#4CAF50';
   if (setSeconds === 0) {
     setSeconds = setSec(convStartTime(seconds));
   }
@@ -50,6 +60,10 @@ function startCountdown() {
     render(setSeconds, 60);
     startBtn.style.display = 'none';
     stopBtn.style.display = 'inline';
+    if (setSeconds === 0) {
+      stopCountdown();
+      progressBar.style.backgroundColor = 'red';
+    }
   }, 1000);
 }
 
@@ -74,13 +88,17 @@ input.addEventListener('input', () => {
 
 input.addEventListener('focus', function () {
   this.select();
-  output.style.cssText = `color: #cccccc;`;
+  output.style.cssText = 'color: #cccccc;';
   cursorElement.className = 'cursor';
 });
 
 input.addEventListener('keyup', ({ key }) => {
-  if (key === 'Enter') startCountdown();
+  if (key === 'Enter') {
+    startCountdown();
+    input.blur();
+  }
 });
+
 startBtn.addEventListener('click', startCountdown);
 stopBtn.addEventListener('click', stopCountdown);
 resetBtn.addEventListener('click', resetCountdown);
