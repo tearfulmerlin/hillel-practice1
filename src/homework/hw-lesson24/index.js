f438f8c42cd6ec09c60949fd0765943d0f4573fe
Volodymyr Vyshniakov <wwwowka@gmail.com>
2023-03-11 14:29:39 +0200

    it's worked

--- src/homework/hw-lesson24/index.js
+++ src/homework/hw-lesson24/index.js
@@ -5,38 +5,46 @@ const stopBtn = document.getElementById('stop');
 const resetBtn = document.getElementById('reset');
 
 let refreshInterval = null;
-let seconds = '500';
-let arrSeconds = ['0', '0', 5];
-let setSeconds = seconds.toString();
 
-function converted() {
+// 598 -> 6m 38s; 128 -> 1m 28s
+const convStartTime = (seconds) => {
+  const arrSeconds = [...seconds.toString().split('').reverse()];
   if (arrSeconds[1] > 5) {
     arrSeconds[1] -= 6;
     arrSeconds[2] ?? (arrSeconds[2] = 0);
     arrSeconds[2] = +arrSeconds[2] + 1;
   }
+
+  return [arrSeconds[0] ?? '  ', (arrSeconds[1] ?? '  '), (arrSeconds[2] ?? '  '), (arrSeconds[3] ?? '  ')];
+};
+
+// sec = s + m * 60
+function setSec(arr) {
+  return +arr[0] + +arr[1] * 10 + +arr[2] * 60 + +arr[3] * 60;
 }
 
-function renderingCountdown() {
-  converted();
-  const countdonwn = `${(arrSeconds[3] ?? '  ') + (arrSeconds[2] ?? '  ')}m ${(arrSeconds[1] ?? '  ') + (arrSeconds[0] ?? '  ')}s`;
-  input.value = seconds;
-  document.title = countdonwn;
-  output.innerText = countdonwn;
-}
-renderingCountdown();
 
-function updateArray(e) {
-  setSeconds = e.target.value;
-  seconds = setSeconds;
-  arrSeconds = seconds.split('').reverse();
+function render(seconds, divider) {
+  const minutes = Math.floor(seconds / divider);
+  let remainingSeconds = seconds % divider;
+  if (remainingSeconds < 10) {
+    remainingSeconds = `0${remainingSeconds}`;
+  }
+
+  output.innerText = `${minutes}m ${remainingSeconds}s`;
 }
+
+render(setSec(convStartTime(input.value)), 60);
+let setSeconds = 0;
+let seconds = input.value;
 
 function startCountdown() {
+  if (setSeconds === 0) {
+    setSeconds = setSec(convStartTime(seconds));
+  }
   refreshInterval = setInterval(() => {
-    seconds -= 1;
-    arrSeconds = seconds.toString().split('').reverse();
-    renderingCountdown();
+    setSeconds -= 1;
+    render(setSeconds, 60);
     startBtn.style.display = 'none';
     stopBtn.style.display = 'inline';
   }, 1000);
@@ -49,14 +57,17 @@ function stopCountdown() {
 }
 
 function resetCountdown() {
-  seconds = setSeconds;
-  arrSeconds = seconds.split('').reverse();
   stopCountdown();
-  renderingCountdown();
+  setSeconds = setSec(convStartTime(input.value));
+  render(setSeconds, 60);
 }
 
-input.addEventListener('input', updateArray);
-input.addEventListener('focusout', renderingCountdown);
+input.addEventListener('input', () => {
+  render(input.value, 100);
+  setSeconds = setSec(convStartTime(input.value));
+  seconds = input.value;
+});
+// input.addEventListener('focusout', renderingCountdown);
 input.addEventListener('keyup', ({ key }) => {
   if (key === 'Enter') startCountdown();
 });
