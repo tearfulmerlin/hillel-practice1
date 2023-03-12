2a7ead79c0f51e7ce25424eb1430e8a76d91c5c2
Hladiy Taras <tarik2002sta@gmail/com>
2023-03-12 22:57:23 +0200

    fix by eslint

--- src/homework/hw-lesson24/script.js
+++ src/homework/hw-lesson24/script.js
@@ -1,72 +1,75 @@
 const startTime = document.querySelector('div.start');
 const stopTime = document.querySelector('div.stop');
 const resetTime = document.querySelector('div.reset');
-let time = document.querySelector('input.time')
+const time = document.querySelector('input.time');
 
 let minutes = 0;
 let seconds = 0;
 
 const defaultTime = 300;
 
-function convertTime(second){
-    const countTime = second;
+function convertTime(second) {
+  const countTime = second;
 
-    seconds = countTime%60
-    minutes = (second - seconds)/60;
+  seconds = countTime % 60;
+  minutes = (second - seconds) / 60;
 }
 
-convertTime(defaultTime)
+convertTime(defaultTime);
 
 
-function showTime(someM,someS){
-    if(someS >= 10) return time.value = `${someM}:${someS}`;
-    if(someS < 10) return time.value = `${someM}:0${someS}`;
+function showTime(someM, someS) {
+  // eslint-disable-next-line no-return-assign
+  if (someS >= 10) return time.value = `${someM}:${someS}`;
+  // eslint-disable-next-line no-return-assign
+  if (someS < 10) return time.value = `${someM}:0${someS}`;
 }
 
 
-showTime(minutes,seconds)
+showTime(minutes, seconds);
 
 let set;
 
-function startTimer(){
-    set = setInterval(() => {
-        if(seconds === 0 && minutes === 0){
-            clearInterval(set);
-        }
-        
-        if(seconds === 0){
-            minutes--;
-            seconds = 59;
-        } else {
-            seconds--;
-        }
-        showTime(minutes,seconds);
-    }, 1000);
+function startTimer() {
+  set = setInterval(() => {
+    if (seconds === 0 && minutes === 0) {
+      clearInterval(set);
+    }
+    if (seconds === 0) {
+      // eslint-disable-next-line no-plusplus
+      minutes--;
+      seconds = 59;
+    } else {
+      // eslint-disable-next-line no-plusplus
+      seconds--;
+    }
+    showTime(minutes, seconds);
+  }, 1000);
 }
 
 startTime.addEventListener('click', startTimer);
 
-function stopTimer(){
-    clearInterval(set)
+function stopTimer() {
+  clearInterval(set);
 }
 
-stopTime.addEventListener('click', stopTimer)
+stopTime.addEventListener('click', stopTimer);
 
-function resetTimer(){
-    clearInterval(set);
-    convertTime(defaultTime);
-    showTime(minutes,seconds);
+function resetTimer() {
+  clearInterval(set);
+  convertTime(defaultTime);
+  showTime(minutes, seconds);
 }
 
 resetTime.addEventListener('click', resetTimer);
 
-function changeTime(){
-    const value = time.value.split(':');
-    if(value.length === 2 && !isNaN(value[0]) && !isNaN(value[1])){
-        minutes = parseInt(value[0]);
-        seconds = parseInt(value[1]);
-        showTime(minutes, seconds);
-    }
+function changeTime() {
+  const value = time.value.split(':');
+  if (value.length === 2 && !isNaN(value[0]) && !isNaN(value[1])) {
+    minutes = parseInt(value[0]);
+    seconds = parseInt(value[1]);
+    showTime(minutes, seconds);
+  }
 }
 
 time.addEventListener('change', changeTime);
