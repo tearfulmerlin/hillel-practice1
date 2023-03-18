c722b6c5809782d8f87015e7e084af20d10a1463
ArtemBorshchev <artemborshchev@icloud.com>
2023-03-18 19:23:38 +0200

    hw23-Borshchev

--- src/homework/hw-lesson-19/index.js
+++ src/homework/hw-lesson-19/index.js
@@ -12,3 +12,29 @@ import assortment from './assortment.js';
  *
  */
 
+const product = document.querySelector('.products');
+const tradeMark = document.querySelector('.trade-mark');
+const models = document.querySelector('.model');
+
+function showProducts(arr, area) {
+  arr.forEach((el) => {
+    const divElem = document.createElement('div');
+    divElem.innerText = el.title;
+    divElem.addEventListener('click', () => {
+      if (el.type === undefined) {
+        showProducts(el.children, tradeMark);
+      }
+      if (el.type === 'trade mark') {
+        showProducts(el.children, models);
+      }
+      if (el.type === 'model') {
+        alert(`${el.title} added to cart`);
+        tradeMark.innerText = null;
+        models.innerText = null;
+      }
+    });
+    area.appendChild(divElem);
+  });
+}
+
+showProducts(assortment, product);
