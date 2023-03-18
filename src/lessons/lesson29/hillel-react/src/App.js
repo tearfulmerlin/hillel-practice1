1b3ed2c0a3320585346944dc49d619094ecc7c2b
oleksii onyshchuk <oleksii.onyshchuk@additiv.com>
2023-03-18 02:18:46 +0200

    intial commit

---
+++ src/lessons/lesson29/hillel-react/src/App.js
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <span>Hello world!</span>
+  );
+}
+
+export default App;
---
+++ src/lessons/lesson29/hillel-react/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(
+    <App />
+);
