d8b43faafa7c1ef63fcb98fd97a939f361b9dcf7
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2023-03-21 09:29:07 +0200

    completed

--- src/homework/hw-lesson20/index.js
+++ src/homework/hw-lesson20/index.js
@@ -14,7 +14,7 @@
 
 
 window.onload = () => {
-    const popUp = window.open("file:///C:/Users/glebk/OneDrive/Рабочий%20стол/kovalchuk/hillel-practice1/src/homework/hw-lesson20/popup.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=800,right=800,width=800,height=800");
+    const popUp = window.open("./popup.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=800,right=800,width=800,height=800");
       
 }
 
