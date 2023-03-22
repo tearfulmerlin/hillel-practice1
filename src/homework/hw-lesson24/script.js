9edd457c440cf762bab18c850de1edcb270ab95e
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2023-03-22 11:47:57 +0200

    completed

--- src/homework/hw-lesson24/script.js
+++ src/homework/hw-lesson24/script.js
@@ -1,43 +1,70 @@
-const start = document.getElementById('start');
-const reset = document.getElementById('reset');
+const timerDisplay = document.querySelector('#timer');
+const startButton = document.querySelector('#start');
+const stopButton = document.querySelector('#stop');
+const resetButton = document.querySelector('#reset');
 
-const hour = document.getElementById("hour");
-const min = document.getElementById("minute");
-const sec = document.getElementById("sec");
+let countdown;
+let timerRunning = false;
+let timeLeft = 300;
 
-let startTimer = null;
+document.title = '5:00 - Countdown Timer'
 
-start.addEventListener('click', function () {
-
-    function startInterval() {
-        startTimer = setInterval(function () {
-            timer();
-        }, 1000);
-    }
-    startInterval();
-});
-
-reset.addEventListener('click', function () {
-    hour.value = 0;
-    min.value = 0;
-    sec.value = 0;
-
-    clearInterval(startTimer);
-});
-
-function timer() {
-    if (hour.value == 0 && min.value == 0 && sec.value == 0) {
-        hour.value = 0;
-        min.value = 0;
-        sec.value = 0;
-    } else if (sec.value != 0) {
-        sec.value--;
-    } else if (min.value != 0 && sec.value == 0) {
-        sec.value = 59;
-        min.value--;
-    } else if (hour.value != 0 && min.value == 0) {
-        min.value = 60;
-        hour.value--;
-    }
-    return;
+function startTimer() {
+    timerRunning = true;
+    countdown = setInterval(() => {
+        timeLeft--;
+        const minutes = Math.floor(timeLeft / 60);
+        let seconds = timeLeft % 60;
+        if (seconds < 10) {
+            seconds = `0${seconds}`;
+        }
+        document.title = `${minutes}:${seconds} - Countdown Timer`;
+        timerDisplay.textContent = `${minutes}:${seconds}`;
+        if (timeLeft === 0) {
+            clearInterval(countdown);
+            document.title = 'Countdown Timer';
+            timerRunning = false;
+        }
+    }, 1000);
 }
+
+function stopTimer() {
+    timerRunning = false;
+    clearInterval(countdown);
+}
+
+function resetTimer() {
+    timerRunning = false;
+    clearInterval(countdown);
+    timeLeft = 300;
+    document.title = 'Countdown Timer';
+    timerDisplay.textContent = '05:00';
+}
+
+function editTimer() {
+    timerDisplay.innerHTML = '<input type="text" id="editTimerInput" />';
+    const editTimerInput = document.querySelector('#editTimerInput');
+    editTimerInput.value = `${Math.floor(timeLeft / 60)}:${timeLeft % 60 < 10 ? '0' : ''}${timeLeft % 60}`;
+    editTimerInput.focus();
+
+    editTimerInput.addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+            const [newMinutes, newSeconds] = editTimerInput.value.split(':');
+            const newTime = (parseInt(newMinutes) * 60) + parseInt(newSeconds);
+            timeLeft = newTime;
+            startTimer();
+        }
+    });
+
+    editTimerInput.addEventListener('blur', function () {
+        const [newMinutes, newSeconds] = editTimerInput.value.split(':');
+        const newTime = (parseInt(newMinutes) * 60) + parseInt(newSeconds);
+        timeLeft = newTime;
+        timerDisplay.innerHTML = `${Math.floor(timeLeft / 60)}:${timeLeft % 60 < 10 ? '0' : ''}${timeLeft % 60}`;
+    });
+}
+
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
+resetButton.addEventListener('click', resetTimer);
+timerDisplay.addEventListener('click', editTimer);
